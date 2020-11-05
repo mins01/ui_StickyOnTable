8e377dfa78046aedafd4506f300288b9e105dbbf
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-11-05 10:17:18 +0900

    최적화 querySelectorAll 보다 .rows나 .cells 를 사용하도록 수정

--- StickyOnTable.js
+++ StickyOnTable.js
@@ -1,4 +1,6 @@
-var StickyOnTable = {
+'use strict';
+
+const StickyOnTable = {
 	"getConfig":function(ta){
 		let left = ta.getAttribute('data-sot-left');
 		if(isNaN(left) || left == null){
@@ -33,42 +35,54 @@ var StickyOnTable = {
 	},
 	"apply":function(ta){
 		let table = ta.querySelector(':scope > table');
-		let rowCellCounts = this.setDataIdxforTable(table)
+		if(!table){
+			console.error("StickyOnTable.apply argument is invalid.");
+			return;
+		}
+		let rowCellCounts = this.getRowCellCounts(table)
+		this.setDataIdxforTable(table,rowCellCounts.rowCount,rowCellCounts.cellCount)
 		this.reset(ta);
 		let conf = Object.assign(this.getConfig(ta),rowCellCounts);
 		conf.left = Math.min(conf.left,conf.cellCount)
 		conf.top = Math.min(conf.top,conf.rowCount)
-		// console.log(conf);
 		this.applySticky(ta,conf);
 	},
 	"reset":function(ta){
-		ta.querySelectorAll(':scope > table > * > tr').forEach((tr, i) => {
+		let table = ta.querySelector(':scope > table');
+		if(!table){
+			console.error("StickyOnTable.reset argument is invalid.");
+			return;
+		}
+		for(const tr of table.rows){
 			for(const td of tr.cells ){
-				td.classList.remove('sot-top','sot-left','sot-right','sot-bottom','sot-cell')
+				td.classList.remove('sot-top','sot-left','sot-right','sot-bottom')
 				td.style.top = null;
 				td.style.left = null;
 				td.style.right = null;
 				td.style.bottom = null;
 			}
-		});
+		};
 	},
-	"setDataIdxforTable":function(table){
+	"getRowCellCounts":function(table){
 		let rowCount = table.rows.length;
 		let cellCount = 0;
-		table.querySelectorAll(':scope > * > tr').forEach((tr, i) => {
+		let tr,td;
+		for(const tr of table.rows){
 			let sum = 0;
-			for(let i=0,m=tr.cells.length;i<m;i++){
-				sum+=tr.cells[i].colSpan;
-				tr.cells[i].removeAttribute('data-row-idx');
-				tr.cells[i].removeAttribute('data-cell-idx');
+			for(const td of tr.cells){
+				sum+=td.colSpan;
 			}
 			cellCount = Math.max(cellCount,sum)
-		});
-		// console.log(rowCount,cellCount);
+		};
+		return {rowCount:rowCount,cellCount:cellCount}
+	},
+	"setDataIdxforTable":function(table,rowCount,cellCount){
 		let arr = new Array(rowCount*cellCount);
-		table.querySelectorAll(':scope > * > tr').forEach((tr, i) => {
+		for(const tr of table.rows){
 			let st = tr.rowIndex * cellCount;
-			for(let td of tr.cells){
+			for(const td of tr.cells){
+				td.removeAttribute('data-row-idx');
+				td.removeAttribute('data-cell-idx');
 				while(st < arr.length && arr[st]!==undefined ){
 					st++
 				}
@@ -83,45 +97,30 @@ var StickyOnTable = {
 					arr[st+cellCount*(i-1)] = td;
 				}
 			}
-		});
-		let dri = 0,dci=0,td;
+		};
 		for(let i=0,m=arr.length;i<m;i++){
-			td = arr[i];
-			dri = Math.floor(i/cellCount);
-			dci = i%cellCount;
+			const td = arr[i];
 			if(td.hasAttribute('data-row-idx')){
 				continue;
 			}
-			td.setAttribute('data-row-idx',dri)
-			td.setAttribute('data-cell-idx',dci)
+			td.setAttribute('data-row-idx',Math.floor(i/cellCount))
+			td.setAttribute('data-cell-idx',i%cellCount)
 		}
-		return {rowCount:rowCount,cellCount:cellCount}
 	},
 	"applySticky":function(ta,conf) {
 		let table = ta.querySelector(':scope > table');
-		// if(ta.scrollWidth == ta.clientWidth  ){
-		// 	table.style.width="auto";
-		// }else{
-		// 	table.style.width="100%";
-		// }
 		let rectTable = table.getBoundingClientRect();
 
-		table.querySelectorAll(':scope > * > tr > td , :scope > * > tr > th').forEach((td, i) => {
-			td.classList.add('sot-cell');
-		});
-
 		for(let i2=0,m2=conf.top;i2<m2;i2++){
-
-			table.rows[i2].querySelectorAll('.sot-cell').forEach((td, i) => {
+			for(let td of table.rows[i2].cells){
 				let rectTd = td.getBoundingClientRect();
 				let top = rectTd.top - rectTable.top;
 				td.classList.add('sot-top');
 				td.style.top = top+'px';
-			});
-
+			};
 		}
 		for(let i2=0,m2=conf.left;i2<m2;i2++){
-			table.querySelectorAll(':scope > * > tr > td[data-cell-idx="'+i2+'"] , :scope > * > tr > th[data-cell-idx="'+i2+'"]').forEach((td, i) => {
+			table.querySelectorAll(':scope td[data-cell-idx="'+i2+'"] , :scope th[data-cell-idx="'+i2+'"]').forEach((td, i) => {
 				let rectTd = td.getBoundingClientRect();
 				let left = rectTd.left - rectTable.left;
 				td.classList.add('sot-left');
